bc8a597a76fbe9efaf27512a8e806beda3288280
dasushan <53913896+dasushan@users.noreply.github.com>
2024-05-19 12:55:24 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const workoutRoutes = require('./routes/workouts');
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use('/api/workouts', workoutRoutes);
+
+// connect to db
+console.log(dotenv);
+mongoose
+  .connect(dotenv.parsed.MONG_URI)
+  .then(() => {
+    console.log('connected to db');
+    // listen for request
+    app.listen(dotenv.parsed.PORT, () => {
+      console.log('Listening on port ', dotenv.parsed.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
