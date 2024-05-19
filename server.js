24a0d97bffc7f93f4918d57fcaac56091769c407
dasushan <53913896+dasushan@users.noreply.github.com>
2024-05-19 17:20:15 +0530

    fixing cors issue

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const workoutRoutes = require('./routes/workouts');
 const app = express();
 
 // middleware
+app.use(cors())
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
